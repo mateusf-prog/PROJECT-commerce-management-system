d21aadb6b0a860184368fd7eab5c074eb743da24
mateusf-prog <mateus102006@hotmail.com>
2023-11-16 20:44:59 -0300

    removing all security configurate before and add new security configuration

---
+++ src/main/java/br/com/mateus/commercemanagementsystem/config/SecurityConfig.java
@@ -0,0 +1,26 @@
+package br.com.mateus.commercemanagementsystem.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http
+                .authorizeHttpRequests((authorize) -> authorize
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(Customizer.withDefaults())
+                .formLogin(Customizer.withDefaults());
+
+        return http.build();
+    }
+}
