e0a5e7cfd12eef9d94689cb3c3cb50395e2b0191
mateusf-prog <mateus102006@hotmail.com>
2024-02-09 10:56:41 -0300

    created more tests for Order resource

--- src/test/java/br/com/mateus/commercemanagementsystem/OrderIT.java
+++ src/test/java/br/com/mateus/commercemanagementsystem/OrderIT.java
@@ -9,14 +9,12 @@ import br.com.mateus.commercemanagementsystem.model.Product;
 import br.com.mateus.commercemanagementsystem.repository.*;
 import com.jayway.jsonpath.JsonPath;
 import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
 import java.math.BigDecimal;
@@ -90,7 +88,32 @@ public class OrderIT {
     }
 
     /**
-     *  This test create an order with invalid cpf, should return status 400 (CPF inválido)
+     *  This test create an order with non-existent customer, should return status 404
+     */
+    @Test
+    public void createOrder_CustomerNotFound_ShouldReturnStatus404() {
+
+        Optional<Product> product = productRepository.findByName("Computer");
+        List<OrderItemDTO> items = new ArrayList<>();
+        items.add(new OrderItemDTO(product.get().getId(), 2));
+
+        String response = testClient
+                .post()
+                .uri("/api/orders")
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(new OrderPostDTO("76453694000", items))
+                .exchange()
+                .expectStatus().isEqualTo(404)
+                .expectBody(String.class)
+                .returnResult().getResponseBody();
+
+        Assertions.assertThat(response).isNotNull();
+        String errorMessage = JsonPath.read(response, "$.message");
+        Assertions.assertThat(errorMessage).isEqualTo("Cliente não encontrado: CPF: 76453694000");
+    }
+
+    /**
+     *  This test create an order with invalid cpf, should return status 400
      */
     @Test
     public void createOrder_InvalidCpf_ShouldReturnStatus400() {
@@ -115,14 +138,13 @@ public class OrderIT {
     }
 
     /**
-     *  This test create an order with empty list items, should return status 400 ("Lista de items não pode ser vazia")
+     *  This test create an order with empty list items, should return status 400
      */
     @Test
-    public void createOrder_WithEmptyListItems_ShouldReturnStatus400() {
+    public void createOrder_EmptyListItems_ShouldReturnStatus400() {
 
         Optional<Product> product = productRepository.findByName("Computer");
         List<OrderItemDTO> items = new ArrayList<>();
-        //items.add(new OrderItemDTO(product.get().getId(), 2));
 
         String response = testClient
                 .post()
@@ -139,4 +161,78 @@ public class OrderIT {
         Assertions.assertThat(errorMessage).isEqualTo("Lista de items não pode ser vazia");
     }
 
+    /**
+     *  This test create an order with invalid items quantity, should return status 400
+     */
+    @Test
+    public void createOrder_InvalidItemsQuantity_ShouldReturnStatus400() {
+
+        Optional<Product> product = productRepository.findByName("Computer");
+        List<OrderItemDTO> items = new ArrayList<>();
+        items.add(new OrderItemDTO(product.get().getId(), 0));
+
+        String response = testClient
+                .post()
+                .uri("/api/orders")
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(new OrderPostDTO("60017663040", items))
+                .exchange()
+                .expectStatus().isEqualTo(400)
+                .expectBody(String.class)
+                .returnResult().getResponseBody();
+
+        Assertions.assertThat(response).isNotNull();
+        String errorMessage = JsonPath.read(response, "$.message");
+        Assertions.assertThat(errorMessage).isEqualTo("Quantidade de itens inválida!");
+    }
+
+    /**
+     *  This test create an order with non-existent products ID, should return status 404
+     */
+    @Test
+    public void createOrder_NonExistentProductID_ShouldReturnStatus404() {
+
+        Optional<Product> product = productRepository.findByName("Computer");
+        List<OrderItemDTO> items = new ArrayList<>();
+        items.add(new OrderItemDTO(50L, 0));
+
+        String response = testClient
+                .post()
+                .uri("/api/orders")
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(new OrderPostDTO("60017663040", items))
+                .exchange()
+                .expectStatus().isEqualTo(404)
+                .expectBody(String.class)
+                .returnResult().getResponseBody();
+
+        Assertions.assertThat(response).isNotNull();
+        String errorMessage = JsonPath.read(response, "$.message");
+        Assertions.assertThat(errorMessage).isEqualTo("Produto não encontrado. ID: 50");
+    }
+
+    /**
+     *  This test create an order with Overstock quantity, should return status 400
+     */
+    @Test
+    public void createOrder_UnavailableStockQuantity_ShouldReturnStatus400() {
+
+        Optional<Product> product = productRepository.findByName("Computer");
+        List<OrderItemDTO> items = new ArrayList<>();
+        items.add(new OrderItemDTO(product.get().getId(), 110));
+
+        String response = testClient
+                .post()
+                .uri("/api/orders")
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(new OrderPostDTO("60017663040", items))
+                .exchange()
+                .expectStatus().isEqualTo(400)
+                .expectBody(String.class)
+                .returnResult().getResponseBody();
+
+        Assertions.assertThat(response).isNotNull();
+        String errorMessage = JsonPath.read(response, "$.message");
+        Assertions.assertThat(errorMessage).isEqualTo("Quantidade indisponível no estoque. Produto: Computer" );
+    }
 }
